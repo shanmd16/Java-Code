92dbcaa29f22b90cbdd26d7a7eeb8713c733e537
shanmd16 <shanmoh1602@gmail.com>
2025-07-29 23:08:58 +0530

    11 ommit

---
+++ src/Exception/BankAccount.java
@@ -0,0 +1,20 @@
+package Exception;
+
+public class BankAccount {
+    private final double balance;
+    private double amount;
+
+    public BankAccount(double amount) {
+        this.balance = amount;
+    }
+
+    public void withdraw(double amount) { throws InsufficientFundsException {
+        if (amount > balance) {
+            throw new InsufficientFundsException(amount);
+
+        }
+        balance -= amount;
+
+    }
+    }
+}
