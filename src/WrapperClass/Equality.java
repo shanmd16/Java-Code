ec9d0a507de46eafe57fa962e3668898cdd46a35
shanmd16 <shanmoh1602@gmail.com>
2025-07-30 07:21:22 +0530

    13

---
+++ src/WrapperClass/Equality.java
@@ -0,0 +1,13 @@
+package WrapperClass;
+
+public class Equality {
+    public static void main(String[] args) {
+        Integer b =1;
+        Integer c =2;
+        int x = 2;
+        int y = 2;
+        System.out.println(x==y);
+        System.out.println(b==c);
+
+    }
+}
