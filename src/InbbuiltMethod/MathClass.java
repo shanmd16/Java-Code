2f11ec5186dc6282ebc2fdf146f49adb66082caf
shanmd16 <shanmoh1602@gmail.com>
2025-07-30 10:26:13 +0530

    14

---
+++ src/InbbuiltMethod/MathClass.java
@@ -0,0 +1,38 @@
+package InbbuiltMethod;
+
+public class MathClass {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 11;
+        // Inbuilt Method
+        int max =Math.max(a, b);
+        int min = Math.max(a, b);
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        int c = -11;
+        Math.abs(c);
+        System.out.println(Math.abs(c));
+        double d =1.12;
+        double ceil = Math.ceil(d);
+        System.out.println(ceil);
+        System.out.println(Math.floor(d));
+        System.out.println(Math.round(d));
+
+
+        int e = 144;
+        System.out.println(Math.sqrt(e));
+        System.out.println(Math.pow(12,2));
+        System.out.println(Math.log(10));
+        System.out.println(Math.log10(10));
+        int t =45;
+        Math.tan(t);
+        System.out.println(Math.random());
+        System.out.println(Math.random()*10);
+        System.out.println(Math.nextAfter(1,4));
+
+
+
+
+
+    }
+}
