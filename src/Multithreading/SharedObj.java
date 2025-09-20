f4dd100287d0f5d6110e1db9afc826ba317e37bc
shanmd16 <shanmoh1602@gmail.com>
2025-09-20 22:26:01 +0530

    19

---
+++ src/Multithreading/SharedObj.java
@@ -0,0 +1,36 @@
+package Multithreading;
+
+ class SharedObj {
+    private volatile boolean flag = false;
+    public void setFlagTrue() {
+        System.out.println("Writer thread made the flag true");
+        flag = true;
+
+    }
+    public void printIfFlagTrue(){
+        while (!flag) {
+
+        }
+        System.out.println("flag is true!");
+
+    }
+
+}
+ class VolatileExample {
+    public static void main(String[] args) {
+        SharedObj sharedObj = new SharedObj();
+        Thread writerThread = new Thread(() -> {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            sharedObj.setFlagTrue();
+
+        });
+        Thread readerThread = new Thread(() -> sharedObj.printIfFlagTrue());
+        writerThread.start();
+        readerThread.start();
+    }
+}
